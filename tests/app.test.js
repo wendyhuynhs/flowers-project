158b010874f29efba2368eb8dd32498da46dd2e3
Wendy Huynh <wendyhuynhs@gmail.com>
2021-02-17 10:03:13 -0800

    testing/app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
 import App from "../public/src/components/App";
 import SearchBar from "../public/src/components/SearchBar";
+import PostList from "../public/src/components/PostList";
 
 const mockStore = configureMockStore([]);
 
@@ -20,6 +21,16 @@ describe("Rendering Components", () => {
     const app = shallow(
       <Provider store={store}>
         <SearchBar />
+      </Provider>
+    );
+    expect(app).toMatchSnapshot();
+  });
+
+  it("renders PostList component correctly", () => {
+    const store = mockStore({});
+    const app = shallow(
+      <Provider store={store}>
+        <PostList />
       </Provider>
     );
     expect(app).toMatchSnapshot();
